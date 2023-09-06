45aed6cb53091214f195f8d450457d723a137e17
adityaapplied <144281600+adityaapplied@users.noreply.github.com>
2023-09-06 13:21:33 -0700

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,32 @@
+@Slf4j
+@RestController
+@RequestMapping("/v1")
+public Class UserController {
+
+  private UserService userService;
+  private UserRepository userRepository;
+
+  @Autowired
+  public UserController(UserService userService, UserRepository userRepository) {
+    this.userService = userService;
+    this.userRepository=userRepository;
+  }
+
+  @PostMapping("/registrations")
+  public ResponseEntity<?> createUser(@RequestBody User user) {
+    User savedUser = UserService.createUser(user);
+    // not needed to send back the entire user as a reponse
+    return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/users/{id}")
+  public ResponseEntity<?> getUserById(@PathVariable("id") UUID id) {
+    Optional<User> optionalUser;
+    try {
+      optionalUser = userRepository.findById(id);
+    } catch (Exception e) {
+      throw new ResourceNotFoundException("User not found");
+    }
+    return optionalUser.get();
+  }
+}
